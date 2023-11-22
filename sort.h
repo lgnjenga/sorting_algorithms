f0a172f38d9294884eea09796962f442c6789368
lgnjenga <lgnjenga@gmail.com>
2023-11-22 05:58:35 +0300

    tasks

--- sort.h
+++ sort.h
@@ -27,6 +27,9 @@ void bubble_sort(int *array, size_t size);
 void swap_ints(int *a, int *b);
 void swap_nodes(listint_t **head, listint_t *node1, listint_t *node2);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void swap_ints(int *a, int *b);
+
 
 
 #endif
